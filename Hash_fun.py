24115f39d66bc275342df530bcbc29841d8b6e29
Omer <omerdayan96@gmail.com>
2017-12-20 20:34:37 +0200

    hash_func(ver.1)
    module of hash_func

---
+++ Hash_fun.py
@@ -0,0 +1,21 @@
+def hash_fun(seed, input, out_len):
+    if len(seed)/len(input) != out_len:
+        print("LENGTHS DOES NOT CONSIST")
+        return
+    len_input = len(input)
+    len_seed = len(seed)
+    output = ""
+    parts_of_seed = [seed[start:start+len_input] for start in range(0, len_seed-1, len_input)]
+    for part in parts_of_seed:
+        tmp_list_output = list(bin(int(part, 2) & int(input, 2))[2:])
+        num = 0
+        for i in tmp_list_output:
+            num ^= int(i)
+        output += str(num)
+    return output
+
+print("hello")
+seed = "0000000100100011010001010110011111111111"
+input = "1110"
+out_len = 10
+print(hash_fun(seed, input, out_len))
