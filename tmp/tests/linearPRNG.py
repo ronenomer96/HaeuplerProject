c1819bd38ad54b5781a85697b61682031ab7e242
RonenV <ronen4822@gmail.com>
2017-12-23 18:09:14 +0200

    linearPRNG V1.2
    Working with good time complexity

--- tmp/tests/linearPRNG.py
+++ tmp/tests/linearPRNG.py
@@ -4,28 +4,34 @@ Input
  desiredLength- length of the desired output
 """
 import numpy as np
+import math
+import datetime
 import random
-c=0
 m=np.int((2**31)-1)
 a=np.int(48271)
 def linearPRNG(seed,desiredLength):
     newString=seed
     listOfStrings=[newString[start:start+32] for start in range(0,len(newString),32)]
-    listOfInts=[]
-    for curStr in listOfStrings:
-        listOfInts.append(int(curStr,2)) 
-    while (len(newString) < desiredLength):
-       tmpList=[]
-       for curVar in listOfInts:
-           tmp=((curVar*a)%m)
-           newString+=np.binary_repr(tmp,32)
-           tmpList.append(tmp)
-       listOfInts+=tmpList
-    return newString
+    numberOfX=len(listOfStrings)
+    aLength=math.ceil(desiredLength/(numberOfX*32))
+    prev=np.int(1)
+    aVec=np.array([])
+    for i in range(0,aLength,1):
+        prev=(a*prev)%m
+        aVec=np.append(aVec,prev)
+    aVec=np.asmatrix(aVec).transpose()
+    xVec=np.array([int(curStr,2) for curStr in listOfStrings])
+    xVec=np.asmatrix(xVec)
+    sol=np.squeeze(np.asarray(((aVec.dot(xVec))%m).flatten().transpose()))
+    strlist=[]
+    for curVal in sol:
+        strlist.append(np.binary_repr(int(curVal),32))
+    return ''.join(strlist)
 
 
-x=random.randint(0,2**100-1)
-y=bin(x)[2:]
-z=linearPRNG(y,1000000)
-print(z)
-print(len(z))    
+x=random.randint(0,2**1000-1)
+y=bin(x)[2:]    
+print(datetime.datetime.now())
+z=linearPRNG(y,10**6)
+print(datetime.datetime.now())
+print(len(z))'   
